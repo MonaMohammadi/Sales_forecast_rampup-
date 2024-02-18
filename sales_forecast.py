ad1f74f7ed563a1cc6a4efa63b774220c57e81ab
Mona Mohammadi <63017685+MonaMohammadi@users.noreply.github.com>
2024-02-17 18:56:50 -0800

    Update sales_forecast.py
    An example of predicting sales based on the sale-advertising file

--- sales_forecast.py
+++ sales_forecast.py
@@ -1,1 +1,17 @@
+# Mona Mohammadi Feb 17th 2021 
+# This code is made from Kaggle code 
+# Import necessary librarires 
 
+
+import numpy as np
+import pandas as pd   #using 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import os 
+import statmodels.formula.api as sm 
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet 
+from sklearn.metrics import mean_squared_error, r2_score 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+import warnings
