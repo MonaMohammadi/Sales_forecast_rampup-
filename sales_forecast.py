5609a87a2e2bf2e6814cc48f790047c738986deb
Mona Mohammadi <63017685+MonaMohammadi@users.noreply.github.com>
2024-02-18 14:05:22 -0800

    Update sales_forecast.py

--- sales_forecast.py
+++ sales_forecast.py
@@ -4,7 +4,7 @@
 
 
 import numpy as np
-import pandas as pd   #using 
+import pandas as pd   #importing for reading CVS files 
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import os 
@@ -14,4 +14,9 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-import warnings
+#import warnings
+
+
+df = pd.read_cvs("C:\Users\Mona\Documents\Datasets\Advertising.cvs")
+df.head()  
+df.columns 
