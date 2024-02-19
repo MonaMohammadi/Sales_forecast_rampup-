aec97d384d89192aa53cfa1f612c692d4ff7109a
Mona Mohammadi <63017685+MonaMohammadi@users.noreply.github.com>
2024-02-18 16:39:12 -0800

    Update sales_forecast.py

--- sales_forecast.py
+++ sales_forecast.py
@@ -4,8 +4,8 @@
 
 
 import numpy as np
-import pandas as pd   #importing for reading CVS files 
-import seaborn as sns 
+import pandas as pd    #for reading CVS file / summurize the CVS file  
+import seaborn as sns  #for scatterplot
 import matplotlib.pyplot as plt 
 import os 
 import statmodels.formula.api as sm 
@@ -20,3 +20,12 @@ from sklearn.model_selection import GridSearchCV
 df = pd.read_cvs("C:\Users\Mona\Documents\Datasets\Advertising.cvs")
 df.head()  
 df.columns 
+df.rename(columns={'Unnamed:0':'Index'}, inpalce= True)
+df.info #check for null values 
+
+#Visualization 
+sns.pairplot(df, x_vars= ["TV", "Radio, "Newspaper", "Sales"], y_vars="Sales", kind= "reg") #Scatterplot to check the linearity assumption 
+df.hist (bins= 24) # histograms to check the normality assumption 
+sns.lmplot( x='TV', y='sales', data=df)
+sns.lmplot( x= 'Radio', y='sales', data=df)
+sns.lmplot(x= 'Newspaper', y='sales', data=df)
